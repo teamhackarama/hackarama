00d5ab1a624086196a097b3e41794086dff2659b
Michael Johnston <mjohnst@microsoft.com>
2018-05-20 08:21:21 +0000

    Post prediction to dynamo db endpoint

---
+++ lamdas/SentimentFunction/src/SentimentFunction/DynamoDbSchema.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace SentimentFunction
+{
+    public class DynamoDbSchema
+    {
+        [JsonProperty("sentiment")]
+        public string Sentiment { get; set; }
+
+        [JsonProperty("fdate")]
+        public string Date { get; set; }
+
+        [JsonProperty("feedback")]
+        public string Feedback { get; set; }
+    }
+}
--- lamdas/SentimentFunction/src/SentimentFunction/Function.cs
+++ lamdas/SentimentFunction/src/SentimentFunction/Function.cs
@@ -1,19 +1,15 @@
 using System;
-using Microsoft.ML.Models;
-using Microsoft.ML.Runtime;
-using Microsoft.ML.Runtime.Api;
-using Microsoft.ML.Trainers;
-using Microsoft.ML.Transforms;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.ML;
 using System.IO;
 using Newtonsoft.Json;
-using System.Text;
-using System.Text.RegularExpressions;
 
 using Amazon.Lambda.Core;
-using System.Diagnostics;
+using Amazon.Lambda;
+using Amazon.Lambda.Model;
+using Amazon;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML;
+using System.Net.Http;
+using System.Text;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -46,7 +42,7 @@ namespace SentimentFunction
         /// <returns></returns>
         public string FunctionHandler(string input, ILambdaContext context)
         {
-            PredictionModel<SentimentData, SentimentPrediction> model = 
+            PredictionModel<SentimentData, SentimentPrediction> model =
                 PredictionModel.ReadAsync<SentimentData, SentimentPrediction>(modelPath).Result;
 
             SentimentData data = new SentimentData
@@ -55,12 +51,59 @@ namespace SentimentFunction
                 Sentiment = 0
             };
             SentimentPrediction prediction = model.Predict(data);
-            string serializedPrediction = JsonConvert.SerializeObject(prediction);
 
-            Console.WriteLine($"Predicition (console): {serializedPrediction}");
-            Trace.WriteLine($"Predicition (trace): {serializedPrediction}");
+            DynamoDbSchema schematizedPrediction = new DynamoDbSchema
+            {
+                Feedback = input,
+                Date = DateTime.UtcNow.ToString("MM/dd/yy"),
+                Sentiment = prediction.Sentiment ? "positive" : "negative"
+            };
+
+            string serializedPrediction = JsonConvert.SerializeObject(schematizedPrediction);
+            string response = PostPredictionViaHttp(serializedPrediction);
+
+            return response;
+            // return PostPredicitionViaInvokeRequest(serializedPrediction);
+        }
+
+        private static string PostPredicitionViaInvokeRequest(string serializedPrediction)
+        {
+            AmazonLambdaClient client = new AmazonLambdaClient("awsaccessKeyID", "awsSecreteAccessKey", RegionEndpoint.USWest1);
+
+            InvokeRequest ir = new InvokeRequest
+            {
+                FunctionName = "HelloLambdaWorld",
+                InvocationType = InvocationType.RequestResponse,
+                Payload = "\"Vikram\""
+            };
+
+            InvokeResponse response = null; // client.Invoke(ir);
+
+            var sr = new StreamReader(response.Payload);
+            JsonReader reader = new JsonTextReader(sr);
+
+            var serilizer = new JsonSerializer();
+            var op = serilizer.Deserialize(reader);
+
+            Console.WriteLine(op);
+            Console.ReadLine();
 
             return serializedPrediction;
+        }
+
+        private static string PostPredictionViaHttp(string input)
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, "https://pt8ktrp8sb.execute-api.us-east-1.amazonaws.com/prod/")
+                {
+                    Content = new StringContent(input, Encoding.UTF8, "application/json")
+                };
+
+                var response = client.SendAsync(request).Result;
+                var content = response.Content.ReadAsStringAsync().Result;
+                return content;
+            }
         }
     }
 }
